15df2b1893f837899b6f9fbe96e14ab9fec7175f
Bobie Fordjour McCamble Kofi <fordjourbobiem@gmail.com>
2024-06-26 18:20:48 +0000

    Refactor import statement for cmd package

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"github.com/Fordjour12/projectGen/cmd"
-)
+import "github.com/Fordjour12/progen/cmd"
 
 func main() {
 	cmd.Execute()
